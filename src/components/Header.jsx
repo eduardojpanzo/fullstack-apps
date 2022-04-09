38eb0c5237cf6da76e1c1050b52ba9f012475a5b
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-09 22:16:42 +0100

    Estrutura do Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return(
+        <header className="jumbotron">
+            <div className="row">
+                <h2>
+                    Crud em ReactJS
+                    <span className="badge badge-secondary">
+                        Eduardo JP
+                    </span>
+                </h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
